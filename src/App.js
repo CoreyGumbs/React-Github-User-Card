6a0d16caf551569cf9230839b6d9634bc619f828
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-14 15:35:44 -0500

    [Add] Added App Class Component and axios call Created ComponentDidMout call with axios. Called to github api for my username, returns data array.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      username: 'CoreyGumbs',
+      userData: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`https://api.github.com/users/${this.state.username}`)
+    .then(res=> {
+      this.setState({userData:[res.data]});
+    })
+    .catch(err => console.log(err));
+  }
+ 
+  
+  render(){
+    console.log(this.state);
+    return(
+      <div>
+        usercard
+      </div>
+    )
+  }
 }
 
 export default App;
