2c6acd8210637f1ac269d3cbe03408490ba92358
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-14 15:53:08 -0500

    [Add] Changed UserSearchBar to a Class componenet Changed from functional to class component Reason: UserSearchBar uses a form for searching. This class will control state to pass up to parent App State for change of username property to search.

--- src/components/UserSearch/UserSearchBar.js
+++ src/components/UserSearch/UserSearchBar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './css/usersearchbar.css';
 
-const UserSearchBar = () => {
-    return(
-        <header className="user-search-bar-container">
-            search bar
-        </header>
-    );
+class UserSearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: ''
+        }
+    }
+
+    render(){
+
+        return(
+            <header className="user-search-bar-container">
+                search bar
+            </header>
+        );
+
+    }
+    
 }
 
 export default UserSearchBar;
